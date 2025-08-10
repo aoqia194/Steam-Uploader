c982a66343eff3e7a55af2bc1e5a9991cefc7130
SirDoggyJvla <simon.cadet79@hotmail.fr>
2025-08-10 19:03:53 +0200

    current compil

--- compil_uploader.sh
+++ compil_uploader.sh
@@ -1,3 +1,3 @@
-g++ -o steam_uploader steam_uploader.cpp \
+g++ -o app *.cpp \
     -I/home/simon/Documents/sdk/public \
     -L/home/simon/Documents/sdk/redistributable_bin/linux64 -lsteam_api -pthread
