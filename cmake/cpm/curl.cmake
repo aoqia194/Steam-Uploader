4d70e6df7e3f825ed081aa8f81c094ea47dd8555
aoqia <aoqia@aoqia.dev>
2025-08-24 01:56:46 +1000

    build: Make curl come from system only

--- cmake/cpm/curl.cmake
+++ cmake/cpm/curl.cmake
@@ -1,16 +1,1 @@
-if (cmake_OS_ARCH)
-    CPMAddPackage(
-            NAME CURL
-            URL "https://curl.se/windows/dl-8.15.0_4/curl-8.15.0_4-${cmake_OS_ARCH}-mingw.zip"
-            VERSION "8.15.0_4"
-    )
-endif ()
-
-if (CURL_ADDED)
-    file(GLOB CURL_STATIC_LIBS "${CURL_SOURCE_DIR}/lib/*.a")
-    add_library(CURL::libcurl INTERFACE IMPORTED)
-    target_include_directories(CURL::libcurl INTERFACE "${CURL_SOURCE_DIR}/include")
-    target_link_libraries(CURL::libcurl INTERFACE "${CURL_STATIC_LIBS}")
-else()
-    find_package(CURL CONFIG REQUIRED)
-endif ()
+find_package(CURL REQUIRED)
