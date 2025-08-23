1b021ccc7042efde58c491581e63655b3f141881
aoqia <aoqia@aoqia.dev>
2025-08-24 01:58:07 +1000

    build: Disable spdlog dllexport warning spam

--- cmake/cpm/spdlog.cmake
+++ cmake/cpm/spdlog.cmake
@@ -1,1 +1,8 @@
-CPMAddPackage(URI "gh:gabime/spdlog@1.15.3" OPTIONS "SPDLOG_BUILD_EXAMPLE OFF")
+CPMAddPackage(
+        URI "gh:gabime/spdlog@1.15.3"
+        OPTIONS
+        "SPDLOG_BUILD_EXAMPLE OFF"
+)
+
+# Disable dllexport warning (it's annoying)
+target_compile_options(spdlog PRIVATE -Wno-ignored-attributes)
