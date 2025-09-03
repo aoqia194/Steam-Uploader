489af6d2bce445e288df74bc27568f9d5de38717
SirDoggyJvla <simon.cadet79@hotmail.fr>
2025-09-04 00:05:01 +0200

    fix: - nuke steamworks file download since it just constantly randomly crashes, and instead package it with the tool - move so files for linux side - potential fix for windows missing packages

--- cmake/cpm/steamworks_sdk.cmake
+++ cmake/cpm/steamworks_sdk.cmake
@@ -12,32 +12,20 @@ else ()
     set(steamworks_sdk_LIB_EXT "dylib")
 endif ()
 
-CPMAddPackage(
-        NAME steamworks_sdk
-        URL https://partner.steamgames.com/downloads/steamworks_sdk_162.zip
-        URL_HASH SHA256=8FF4D29DDDD225718164B9279DCFDE5EED1DB26FEE5DC49DCCA4906C7F1F0C1A
+# Use local Steamworks SDK directory
+set(steamworks_sdk_SOURCE_DIR "${CMAKE_SOURCE_DIR}/packages/steam")
+
+add_library(steamworks_sdk SHARED IMPORTED)
+
+set(steamworks_sdk_LIBDIR "${steamworks_sdk_SOURCE_DIR}/redistributable_bin/${steamworks_sdk_OS_ARCH}")
+set(steamworks_sdk_LIBFILE "${steamworks_sdk_LIBDIR}/${steamworks_sdk_LIB_NAME}.${steamworks_sdk_LIB_EXT}")
+set_target_properties(steamworks_sdk PROPERTIES
+        IMPORTED_LOCATION ${steamworks_sdk_LIBFILE}
+        INTERFACE_INCLUDE_DIRECTORIES "${steamworks_sdk_SOURCE_DIR}/public"
 )
-if (steamworks_sdk_ADDED)
-    add_library(steamworks_sdk SHARED IMPORTED)
 
-    set(steamworks_sdk_LIBDIR "${steamworks_sdk_SOURCE_DIR}/redistributable_bin/${steamworks_sdk_OS_ARCH}")
-    set(steamworks_sdk_LIBFILE "${steamworks_sdk_LIBDIR}/${steamworks_sdk_LIB_NAME}.${steamworks_sdk_LIB_EXT}")
+if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
     set_target_properties(steamworks_sdk PROPERTIES
-            IMPORTED_LOCATION ${steamworks_sdk_LIBFILE}
-            INTERFACE_INCLUDE_DIRECTORIES "${steamworks_sdk_SOURCE_DIR}/public"
+            IMPORTED_IMPLIB "${steamworks_sdk_LIBDIR}/${steamworks_sdk_LIB_NAME}.lib"
     )
-
-    if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
-        set_target_properties(steamworks_sdk PROPERTIES
-                IMPORTED_IMPLIB "${steamworks_sdk_LIBDIR}/${steamworks_sdk_LIB_NAME}.lib"
-        )
-    endif ()
-
-    # Copy the library manually to the bin folder.
-    # This is required for only steamworks sdk since it's an IMPORTED library.
-#    add_custom_command(TARGET "${CMAKE_PROJECT_NAME}" POST_BUILD
-#            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-#            "${steamworks_sdk_LIBFILE}"
-#            "$<TARGET_FILE_DIR:${CMAKE_PROJECT_NAME}>"
-#    )
 endif ()
