9d7b316557c86174407e975fd197dd062682ec29
aoqia <aoqia@aoqia.dev>
2025-09-08 04:08:04 +1000

    build: Swap CPM for VCPKG

--- cmake/cpm/spdlog.cmake
+++
@@ -1,8 +0,0 @@
-CPMAddPackage(
-        URI "gh:gabime/spdlog@1.15.3"
-        OPTIONS
-        "SPDLOG_BUILD_EXAMPLE OFF"
-)
-
-# Disable dllexport warning (it's annoying)
-target_compile_options(spdlog PRIVATE -Wno-ignored-attributes)
